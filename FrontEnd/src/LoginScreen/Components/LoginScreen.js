3d3df4fb9109c8aa1b73065776780fa679862542
elijahcano33 <elijah.c104777@gmail.com>
2020-09-18 18:34:39 -0400

    successfully setup auth with google

--- FrontEnd/src/LoginScreen/Components/LoginScreen.js
+++ FrontEnd/src/LoginScreen/Components/LoginScreen.js
@@ -1,26 +1,73 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import styles from '../Styles/LoginScreenStyles.js';
-import { LoginButton, AccessToken } from 'react-native-fbsdk';
+import { LoginManager } from 'react-native-fbsdk'
+import { GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
+import Entypo from 'react-native-vector-icons/Entypo';
+
 
 export default class LoginScreen extends Component{
-    
+    constructor(props){
+        super();
+        this.state = {
+            isSigninInProgress:false,
+            userInfo:null
+        }
+
+        GoogleSignin.configure({
+            scopes: [],
+            webClientId: 'add yor webclient here',
+            offlineAccess: true, 
+            hostedDomain: '', 
+            forceConsentPrompt: true,
+            accountName: ''
+        });
+    }
+
+
+    handleGoogleLogin = async () => {
+        try {
+           await GoogleSignin.hasPlayServices();
+           const userInfo = await GoogleSignin.signIn();
+            console.log('_____userinfo',userInfo)
+            this.setState({ userInfo });
+        } catch (error) {
+            console.log(error)
+        }
+   }
+
+   handleFacebookLogin () {
+        LoginManager.logInWithPermissions(['public_profile', 'email', 'user_friends']).then(
+        function (result) {
+            if (result.isCancelled) {
+            console.log('Login cancelled')
+            } else {
+            console.log('Login success with permissions: ' + result.grantedPermissions.toString())
+            }
+        },
+        function (error) {
+            console.log('Login fail with error: ' + error)
+        }
+        )
+    }
+
+
     render() {
         return (
             <View style={styles.container}>
-                <LoginButton
-                    onLoginFinished={(error, result) => {
-                        if (error) {
-                            console.log("login has error: " + result.error);
-                        }else if (result.isCancelled) {
-                            console.log("login is cancelled.");
-                        } else {
-                            AccessToken.getCurrentAccessToken().then(
-                            (data) => {console.log(data.accessToken.toString())
-                            })}
-                        }
-                    }
-                    onLogoutFinished={() => console.log("logout.")} />
+                <TouchableOpacity style={styles.facebookSignInButton} onPress={this.handleFacebookLogin}>
+                    <Entypo name="facebook" size={30} style={styles.facebookIcon}/>
+                    <Text style={styles.signInWithFacebookText}>Sign in with Facebook </Text>          
+                </TouchableOpacity>
+
+                <GoogleSigninButton
+                    style={styles.googleSignInButton}
+                    size={GoogleSigninButton.Size.Wide}
+                    color={GoogleSigninButton.Color.Dark}
+                    onPress={()=>{this.handleGoogleLogin()}}
+                    disabled={this.state.isSigninInProgress} 
+                />
+
             </View>
         );
     }
--- FrontEnd/src/LoginScreen/Styles/LoginScreenStyles.js
+++ FrontEnd/src/LoginScreen/Styles/LoginScreenStyles.js
@@ -1,6 +1,5 @@
 import {StyleSheet} from 'react-native';
 
-
 export default StyleSheet.create({
     container: {
         backgroundColor: 'white',
@@ -8,5 +7,30 @@ export default StyleSheet.create({
         alignItems: 'center', 
         flex: 1
     },
+
+    googleSignInButton: {
+        width: '51%', 
+        height: '5%'
+    },
+
+    facebookSignInButton: {
+        width: '49%', 
+        height: '4.5%',
+        backgroundColor: '#151515'
+    },
+
+    signInWithFacebookText: {
+        color: 'white',
+        top: '15%',
+        alignSelf: 'center',
+        left: '20%',
+        position: 'absolute',
+        fontWeight: 'bold'
+    },
+
+    facebookIcon: {
+        color: 'white',
+        width: '50%'
+    }
     
 });
