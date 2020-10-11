5800bdfa790188223318c13d4b838805b22e63b4
Camryn Buonamassa <camrynbuonamassa@gmail.com>
2020-10-11 18:59:19 -0400

    added mapscreen to Elijah's branch

--- FrontEnd/src/MapScreen/Components/MapScreen.js
+++ FrontEnd/src/MapScreen/Components/MapScreen.js
@@ -1,23 +1,70 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, SafeAreaView, Platform, PermissionsAndroid} from 'react-native';
 import styles from '../Styles/MapScreenStyles.js';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import MapView from 'react-native-maps';
+import SearchBar from 'react-native-search-bar';
 
 export default class MapScreen extends Component{
-
-    jumpToCameraScreen () {
-        this.props.navigation.navigate('CameraScreen');
+    constructor(props){
+        super();
+        if (Platform.OS === "android") {
+            this.requestLocationPermission();
+        }
+        this.state = {
+            region:{ 
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }
+        }
     }
+
+    async requestLocationPermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+              title: "Diggraffiti",
+              message: "Allow Digraffiti to access your location"
+            }
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            alert("Location access granted");
+          } else {
+            alert("Location access denied");
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+    }
+
+    onRegionChange = (region) => {
+        this.setState({ region }, () => {
+            //console.log("HERE", this.state.region);
+        });
+    };
 
     render() {
         return (
-            <View style={styles.container}>
-                <TouchableOpacity style={styles.cameraIcon} onPress={() => this.jumpToCameraScreen()} >
-                    <AntDesign name="camera" size={30} />       
-                </TouchableOpacity>
-                <Text>Map Screen</Text>
+           <SafeAreaView style={{flex: 1}}>
+             {/* <SearchBar
+               ref='searchBar'
+               placeholder='Search'
+             /> */}
+             <View style={{ flex: 1}}>
+            <MapView 
+                onRegionChange={this.onRegionChange}
+                style={{flex: 1}}        
+                zoomEnabled={true}  
+                followsUserLocation={true}
+                showsMyLocationButton={false}     
+                showsUserLocation={true}
+            />
             </View>
+           </SafeAreaView>
         );
     }
 }
+    
     
--- FrontEnd/src/MapScreen/Styles/MapScreenStyles.js
+++ FrontEnd/src/MapScreen/Styles/MapScreenStyles.js
@@ -2,15 +2,9 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
     container: {
-        backgroundColor: '#be0000',
+        backgroundColor: 'white',
         justifyContent: 'center', 
         alignItems: 'center', 
         flex: 1
-    },  
-
-    cameraIcon: {
-        position: 'absolute',
-        left: '10%',
-        top: '10%',
     }
 });
