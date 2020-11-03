05a8616a876c4818de3b278546bbe2357eb6339c
elijahcano33 <elijah.c104777@gmail.com>
2020-11-02 23:32:14 -0500

    brought sketches to camera screen successfully

--- FrontEnd/src/CameraScreen/Components/CameraScreen.js
+++ FrontEnd/src/CameraScreen/Components/CameraScreen.js
@@ -1,18 +1,22 @@
 import React, {Component} from 'react';
 import {View, Text, TouchableOpacity, PermissionsAndroid} from 'react-native';
 import styles from '../Styles/CameraScreenStyles.js';
-import Fontisto from 'react-native-vector-icons/Fontisto';
 import {RNCamera} from 'react-native-camera';
+import RNSketchCanvas from '@terrylinla/react-native-sketch-canvas';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Entypo from 'react-native-vector-icons/Entypo';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export default class CameraScreen extends Component {
-  jumpToMapScreen() {
-    this.props.navigation.navigate('MapScreen');
-  }
-
   constructor() {
     super();
     if (Platform.OS === 'android') {
       this.requestCameraPermission();
+    }
+
+    this.state = {
+      pressed: false
     }
   }
 
@@ -35,10 +39,17 @@ export default class CameraScreen extends Component {
     }
   }
 
+  jumpToMapScreen() {
+    this.props.navigation.navigate('MapScreen');
+  }
+
+  paintBrushPressed(){
+    this.setState({pressed: !this.state.pressed});
+  }
+
   render() {
     return (
       <View style={styles.container}>
-      
         <RNCamera
           ref={(ref) => {
             this.CameraScreen = ref;
@@ -48,17 +59,45 @@ export default class CameraScreen extends Component {
           flashMode={RNCamera.Constants.FlashMode.auto}
           captureAudio={false}>
 
-          <TouchableOpacity style={styles.mapMarkerIcon}>
-            <Fontisto
-              name="map-marker-alt"
-              size={30}
-              onPress={() => this.jumpToMapScreen()}
+          {this.state.pressed ?
+            <RNSketchCanvas
+              defaultStrokeIndex={0}
+              defaultStrokeWidth={5}
+              containerStyle={styles.sketchContainer}
+              canvasStyle={styles.sketchCanvas}
+              closeComponent={<View style={styles.close}><MaterialCommunityIcons name="window-close" size={40}/></View>}
+              onClosePressed={() => this.paintBrushPressed()}
+              clearComponent={<View style={styles.trash}><Fontisto name="trash" size={35}/></View>}
+              eraseComponent={<View style={styles.eraser}><MaterialCommunityIcons name="eraser" size={45} /></View>}
+              strokeComponent={color => (<View style={[{ backgroundColor: color }, styles.strokeColorButton]}/>)}
+              strokeSelectedComponent={(color, index, changed) => {
+              return (<View style={[{backgroundColor: color, borderWidth: 2 }, styles.strokeColorButton]}/>)}}
+              strokeWidthComponent={(w) => {
+              return (<View style={styles.strokeWidthButton}>
+                          <View  style={{backgroundColor: 'white', marginHorizontal: 2.5,width: Math.sqrt(w / 3) * 10, height: Math.sqrt(w / 3) * 10, borderRadius: Math.sqrt(w / 3) * 10 / 2 }} />
+                      </View>
+              )}}
+              saveComponent={<View style={styles.save}><Entypo name="save" size={40} style={styles.facebookIcon}/></View>}
+              savePreference={() => {
+              return {
+                folder: 'RNSketchCanvas',
+                filename: String(Math.ceil(Math.random() * 100000000)),
+                transparent: false,
+                imageType: 'png'
+              }}}
             />
-          </TouchableOpacity>
+            : 
+            <View >
+              <TouchableOpacity style={styles.mapMarkerIcon}>
+                <Fontisto name="map-marker-alt" size={30} onPress={() => this.jumpToMapScreen()}/>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.paintBrush} onPress={() => this.paintBrushPressed()}>
+                <FontAwesome5 name="paint-brush" size={50}/>
+              </TouchableOpacity>
+            </View>
+            }
         </RNCamera>
-
-
-        
       </View>
     );
   }
