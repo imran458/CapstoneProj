f245dd33f29796338932028dab714eeaa4ae859e
elijahcano33 <elijah.c104777@gmail.com>
2020-11-15 02:08:14 -0500

    cleaned up camera screen code

--- FrontEnd/src/CameraScreen/Components/CameraScreen.js
+++ FrontEnd/src/CameraScreen/Components/CameraScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, Alert, TouchableOpacity, PermissionsAndroid, Platform, Image} from 'react-native';
+import {View, TouchableOpacity, PermissionsAndroid, Platform, Image} from 'react-native';
 import styles from '../Styles/CameraScreenStyles.js';
 import {RNCamera} from 'react-native-camera';
 import RNSketchCanvas from '@terrylinla/react-native-sketch-canvas';
@@ -7,8 +7,7 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import Entypo from 'react-native-vector-icons/Entypo';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import CameraRoll from "@react-native-community/cameraroll";
-import { captureScreen } from "react-native-view-shot";
+import ViewShot, { captureScreen } from "react-native-view-shot";
 
 export default class CameraScreen extends Component {
   constructor() {
@@ -16,6 +15,7 @@ export default class CameraScreen extends Component {
     if (Platform.OS === 'android') {
       this.requestCameraPermission();
     }
+    this.viewShotRef = React.createRef();
 
     this.state = {
       pressed: false,
@@ -24,27 +24,6 @@ export default class CameraScreen extends Component {
       imageSaved: false
     }
   }
-
-
-  async hasAndroidPermission() {
-    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
-  
-    const hasPermission = await PermissionsAndroid.check(permission);
-    if (hasPermission) {
-      return true;
-    }
-  
-    const status = await PermissionsAndroid.request(permission);
-    return status === 'granted';
-  }
-  
-  async savePicture() {
-    if (Platform.OS === "android" && !(await hasAndroidPermission())) {
-      return;
-    }
-  
-    CameraRoll.save(tag, { type, album })
-  };
 
   async requestCameraPermission() {
     try {
@@ -55,11 +34,7 @@ export default class CameraScreen extends Component {
           message: 'Let Diggrafiti Access the Camera',
         },
       );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('You can use the camera');
-      } else {
-        console.log('Camera permission denied');
-      }
+      granted === PermissionsAndroid.RESULTS.GRANTED ? console.log('You can use the camera') : console.log('Camera permission denied');
     } catch (err) {
       console.warn(err);
     }
@@ -73,32 +48,37 @@ export default class CameraScreen extends Component {
     this.setState({pressed: !this.state.pressed});
   }
 
-  captureScreenFunction (){
-
+  captureScreen(){
+    this.ref.viewShot.capture().then(uri => {
+      console.log("do something with ", uri);
+      this.setState({imageURI: uri});
+   });
+   
+   /*
+    The following code successfully takes a screenshot of the current camera view,
+    but doesn't get any of the sketches
+    
     captureScreen({
       format: "jpg",
       quality: 0.8
     })
     .then(
       uri => this.setState({ imageURI : uri }),
-      error => console.error("Oops, Something Went Wrong", error)
+      error => console.error("Something Went Wrong", error)
       
     );
-
+    */
     console.log("This is where the saved image is located: " + this.state.imageURI);
     this.setState({imageSaved: true});
-
   }
 
   render() {
     return (
-      <View style={styles.container}>
+      <ViewShot style={styles.container} ref="viewShot" options={{ format: "jpg", quality: 0.9 }}>
          
         <RNCamera
-          ref={(ref) => {
-            this.CameraScreen = ref;
-          }}
-          style={{position: 'absolute',top: 0,left: 0,right: 0, bottom: 0}}
+          ref={(ref) => {this.CameraScreen = ref;}}
+          style={styles.cameraView}
           type={RNCamera.Constants.Type.back}
           flashMode={RNCamera.Constants.FlashMode.auto}
           captureAudio={false}>
@@ -110,54 +90,30 @@ export default class CameraScreen extends Component {
               defaultStrokeWidth={5}
               containerStyle={styles.sketchContainer}
               canvasStyle={styles.sketchCanvas}
-              closeComponent={<View style={styles.close}><MaterialCommunityIcons name="window-close" size={40}/></View>}
               onClosePressed={() => this.paintBrushPressed()}
+              closeComponent={<View style={styles.close}><MaterialCommunityIcons name="window-close" size={40}/></View>}
               clearComponent={<View style={styles.trash}><Fontisto name="trash" size={35}/></View>}
               eraseComponent={<View style={styles.eraser}><MaterialCommunityIcons name="eraser" size={45} /></View>}
               strokeComponent={color => (<View style={[{ backgroundColor: color }, styles.strokeColorButton]}/>)}
-              strokeSelectedComponent={(color, index, changed) => {
-              return (<View style={[{backgroundColor: color, borderWidth: 2 }, styles.strokeColorButton]}/>)}}
-              strokeWidthComponent={(w) => {
-              return (<View style={styles.strokeWidthButton}>
-                          <View  style={{backgroundColor: 'white', marginHorizontal: 2.5,width: Math.sqrt(w / 3) * 10, height: Math.sqrt(w / 3) * 10, borderRadius: Math.sqrt(w / 3) * 10 / 2 }} />
-                      </View>
-              )}}
               saveComponent={<View style={styles.save}><Entypo name="save" size={40} style={styles.facebookIcon}/></View>}
-              savePreference={() => {
-              
-              return {
-                folder: null, 
-                filename: String(Math.ceil(Math.random() * 100000000)),
-                transparent: false,
-                includeImage: true,
-                imageType: 'png'
-              }}}
-
-              onSketchSaved={()=> this.captureScreenFunction()}
-              
+              savePreference={() => {return {folder: null, filename: String(Math.ceil(Math.random() * 100000000)),transparent: false, includeImage: true, imageType: 'jpg'}}}
+              onSketchSaved={()=> this.captureScreen()} 
+              strokeSelectedComponent={(color) => {return (<View style={[{backgroundColor: color, borderWidth: 2 }, styles.strokeColorButton]}/>)}}
+              strokeWidthComponent={(w) => {return (<View style={styles.strokeWidthButton}><View  style={{backgroundColor: 'white', marginHorizontal: 2.5,width: Math.sqrt(w / 3) * 10, height: Math.sqrt(w / 3) * 10, borderRadius: Math.sqrt(w / 3) * 10 / 2 }} /></View>)}}
             />
             : 
-            <View >
+            <View>
               <TouchableOpacity style={styles.mapMarkerIcon}>
                 <Fontisto name="map-marker-alt" size={42} onPress={() => this.jumpToMapScreen()}/>
               </TouchableOpacity>
-
               <TouchableOpacity style={styles.paintBrush} onPress={() => this.paintBrushPressed()}>
                 <FontAwesome5 name="paint-brush" size={50}/>
               </TouchableOpacity>
-
-              
             </View>
-
             }
-            
         </RNCamera>
-        {this.state.imageSaved && this.state.imageURI !== '' ? 
-           
-            <Image style={{width: 50,
-              height: 50,}} source={{uri: this.state.imageURI}}/>
-          : null}
-      </View>
+        {this.state.imageSaved && this.state.imageURI !== '' ?  <Image style={{width: 200, height: 200,}} source={{uri: this.state.imageURI}}/> : null}
+      </ViewShot>
     );
   }
 }
