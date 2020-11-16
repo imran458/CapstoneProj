6fcacb1440507d403f3245cf5165cd7b40abe60c
Camryn Buonamassa <camrynbuonamassa@gmail.com>
2020-11-16 03:03:54 -0500

    added scrolling functionality to search bar drop-down list

--- FrontEnd/src/MapScreen/Components/MapScreen.js
+++ FrontEnd/src/MapScreen/Components/MapScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity, SafeAreaView, Platform, PermissionsAndroid} from 'react-native';
+import {View, Text, TouchableOpacity, SafeAreaView, FlatList, ListItem, Platform, PermissionsAndroid} from 'react-native';
 import styles from '../Styles/MapScreenStyles.js';
 import MapView, { Marker } from 'react-native-maps';
 import SearchBar from 'react-native-search-bar';
@@ -13,9 +13,11 @@ export default class MapScreen extends Component{
             this.requestLocationPermission();
         }
         this.state = {
-            region:{ 
+            region: { 
                 latitude: 37.78825,
-                longitude: -122.4324
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
             },
             search: "",
             predictions: [],
@@ -25,7 +27,8 @@ export default class MapScreen extends Component{
               latitudeDelta:.1,
               longitudeDelta: .1
             }},
-            bottomMargin: 1
+            bottomMargin: 1,
+            predictionsArrayEmpty: true
         }
         this.handleChangeTextDebounced = _.debounce(this.handleChangeText, 1000);
     }
@@ -56,6 +59,7 @@ export default class MapScreen extends Component{
     };
 
     async handleChangeText(text){
+      this.setState({predictionsArrayEmpty: !this.state.predictionsArrayEmpty});
       this.setState({ search: text }, () => {
         console.log(this.state.search)
       });
@@ -64,14 +68,13 @@ export default class MapScreen extends Component{
       try{
         const result = await fetch(url);
         const json =  await result.json();
-        //console.log(json); //here
+        console.log(json.predictions.length);
         this.setState( {predictions: json.predictions} );
       } catch(err){
         console.log(err);
       }
     }
 
-    //button to go back to current location???
     async handleSelectedAddress(prediction){
       const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${prediction['place_id']}&key=${env.API_KEY}`
       try{
@@ -84,11 +87,13 @@ export default class MapScreen extends Component{
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }});
-        this.setState( {marker: { 
-          latitude: json.result.geometry.location.lat,
-          longitude: json.result.geometry.location.lng,
-          latitudeDelta:.1,
-          longitudeDelta: .1
+        this.setState( {
+          //search: "",
+          marker: { 
+            latitude: json.result.geometry.location.lat,
+            longitude: json.result.geometry.location.lng,
+            latitudeDelta:.1,
+            longitudeDelta: .1
         }}, () => {
           //console.log("MARKER", this.state.marker);
         });
@@ -97,69 +102,61 @@ export default class MapScreen extends Component{
       }
     }
 
+    renderPrediction = ({ item }) => {
+      return (
+        <TouchableOpacity style={styles.prediction} onPress={() => this.handleSelectedAddress(item)}>
+          <Text style={styles.text}>
+            {item.description}
+          </Text>
+          <View style={styles.divider}/>
+        </TouchableOpacity>
+      );
+    };
+
     render() {
-        //console.log(this.state.predictions)
-        const predictions = this.state.predictions.map((prediction, index) => (
-          <TouchableOpacity key={index} style={styles.container} onPress={() => this.handleSelectedAddress(prediction)}>
-            <View>
-              <Text style={styles.text}>
-                {prediction.description}
-              </Text>
-            </View>
-          </TouchableOpacity>
-        ));
-        // if(!this.state.marker.longitude){
-        //   return (
-        //     <SafeAreaView style={{flex: 1}}>
-        //      <SearchBar
-        //        value={this.state.search}
-        //        onChangeText={text=> this.onTextChange(text)}
-        //        //make sure text is updating before api call is made
-        //        placeholder='Search'
-        //      />
-        //      {predictions}
-        //      <MapView 
-        //        region={this.state.region}
-        //        onRegionChange={this.onRegionChange}
-        //        style={{flex: 1, marginBottom: this.state.bottomMargin}}        
-        //        zoomEnabled={true}  
-        //        followsUserLocation={true}
-        //        showsMyLocationButton={true}     
-        //        showsUserLocation={true}
-        //        annotations={this.state.marker} 
-        //        onMapReady={() => this.setState({ bottomMargin: 0 })} 
-        //      >
-        //      </MapView>
-        //     </SafeAreaView>
-        //  );
-        // }
-        //else{
-          return (
-            <SafeAreaView style={{flex: 1}}>
-              <SearchBar
-                value={this.state.search}
-                onChangeText={text=>
-                  this.handleChangeTextDebounced(text)
-                }
-              />
-              {predictions}
-              <MapView 
-                region={this.state.region}
-                onRegionChange={this.onRegionChange}
-                style={{flex: 1, marginBottom: this.state.bottomMargin}}        
-                zoomEnabled={true}  
-                followsUserLocation={true}
-                showsMyLocationButton={true}     
-                showsUserLocation={true}
-                annotations={this.state.marker} 
-                onMapReady={() => this.setState({ bottomMargin: 0 })} 
-              >
-                <Marker coordinate={{ latitude: this.state.marker.latitude, longitude: this.state.marker.longitude }} />
-              </MapView>
-            </SafeAreaView>
-          );
-        //}
+        return (
+          <SafeAreaView style={styles.container}>
+          <SearchBar
+              value={this.state.search}
+              onChangeText={text=>
+                this.handleChangeTextDebounced(text)
+              }
+              textColor='black'  
+            />
+            {this.state.search === "" ? 
+              <View style={styles.container}>
+                <FlatList 
+                  style={styles.flatList1}
+                  data={this.state.predictions}
+                  renderItem={this.renderPrediction}
+                />
+                <MapView 
+                  region={this.state.region}
+                  style={styles.map1}
+                  zoomEnabled={true}  
+                  followsUserLocation={true}
+                  showsMyLocationButton={true}     
+                  showsUserLocation={true}
+                  annotations={this.state.marker} 
+                /> 
+              </View>: 
+              <View style={styles.container}>
+                <FlatList 
+                  style={styles.flatList2}
+                  data={this.state.predictions}
+                  renderItem={this.renderPrediction}
+                />
+                <MapView 
+                    region={this.state.region}
+                    style={styles.map2}
+                    zoomEnabled={true}  
+                    followsUserLocation={true}
+                    showsMyLocationButton={true}     
+                    showsUserLocation={true}
+                    annotations={this.state.marker} 
+                />
+              </View>}
+          </SafeAreaView>
+        );
     }
 }
-    
-    
